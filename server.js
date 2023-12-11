da563051b85e8b40ba8db957468a8787ddf318e3
cabilesc <144515905+cabilesc@users.noreply.github.com>
2023-12-11 11:21:42 -0800

    new

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
+
+function logger(req, res, next) {
+    console.log(req.orignalUrl)
+    next()
+}
+
+app.listen(3000)
